4c473d3c755fe2c964500cba0fc79ae67c63064a
unknown <haha950903@naver.com>
2020-03-12 13:20:46 +0900

    Updates

---
+++ precache-manifest.3ba66dee73c4f7d05790347100069b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50b10c39d2a9ecbf231a7a6a7227b79",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4880c2a2bfd615e08590",
+    "url": "/movie_app/static/css/main.9c0e7eb0.chunk.css"
+  },
+  {
+    "revision": "cada8706178801cbd9bd",
+    "url": "/movie_app/static/js/2.c59935f1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.c59935f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4880c2a2bfd615e08590",
+    "url": "/movie_app/static/js/main.267f6efe.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
